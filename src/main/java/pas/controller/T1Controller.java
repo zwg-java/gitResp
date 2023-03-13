e0e9104bfe1098bfcce0c6b4ce00b952bfd4c718
zwg-java <76099201+zwg-java@users.noreply.github.com>
2023-03-13 22:55:58 +0800

    T1 T2

--- src/main/java/pas/controller/T1Controller.java
+++ src/main/java/pas/controller/T1Controller.java
@@ -1,4 +1,8 @@
 package pas.controller;
 
 public class T1Controller {
+    public static void main(String[] args) {
+        System.out.println("测试");
+        System.out.println("测试2");
+    }
 }
--- src/main/java/pas/controller/T2Controller.java
+++ src/main/java/pas/controller/T2Controller.java
@@ -4,5 +4,6 @@ package pas.controller;
 public class T2Controller {
     public static void main(String[] args) {
         System.out.println("测试");
+        System.out.println("测试2");
     }
 }
