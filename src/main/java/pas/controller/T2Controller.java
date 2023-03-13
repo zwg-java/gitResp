f5aab0910e0ff4e8c954d98e6cdd8f97fce00788
zwg-java <76099201+zwg-java@users.noreply.github.com>
2023-03-13 22:57:26 +0800

    Update T2Controller.java

--- src/main/java/pas/controller/T2Controller.java
+++ src/main/java/pas/controller/T2Controller.java
@@ -5,5 +5,6 @@ public class T2Controller {
     public static void main(String[] args) {
         System.out.println("测试");
         System.out.println("测试2");
+         System.out.println("测试3");
     }
 }
